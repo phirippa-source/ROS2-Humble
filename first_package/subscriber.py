c80c6f8621d65652a101f5a44316b703ee7e3f03
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-09-12 16:07:53 +0900

    Create subscriber.py

---
+++ first_package/subscriber.py
@@ -0,0 +1,22 @@
+import rclpy as rp
+from rclpy.node import Node
+from turtlesim.msg import Pose			# /turtle1/pose [turtlesim/msg/Pose]
+
+class TurtlesimSubscriber(Node):
+	def __init__(self):
+		super().__init__('turtlesim_subscriber')
+		self.subscriber = self.create_subscription(Pose, '/turtle1/pose', self.callback, 10)
+
+	def callback(self, msg):
+		print('x:', msg.x, ', y:', msg.y)
+
+def main(args=None):
+	rp.init(args=args)
+	node_subscriber = TurtlesimSubscriber()
+	rp.spin(node_subscriber)
+
+	node_subscriber.destroy_node()
+	rp.shutdown()
+
+if __name__ == '__main__':
+	main()
